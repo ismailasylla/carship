80730c7609fb0db0969ca00b7544a75c8b97fca1
Sylla <ismailasyllaa@gmail.com>
2024-07-23 20:27:46 +0400

    Add client

---
+++ client/src/App.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+import HomePage from "./components/pages/HomePage";
+import LoginPage from "./components/pages/LoginPage";
+import RegisterPage from "./components/pages/RegisterPage";
+import NavBar from "./components/NavBar";
+
+const App: React.FC = () => {
+  return (
+    <Provider store={store}>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+      </Router>
+    </Provider>
+  );
+};
+
+export default App;
