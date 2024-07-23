1b550658693d9bd854bf803d4f8eb7920fb87948
Sylla <ismailasyllaa@gmail.com>
2024-07-23 21:44:52 +0400

    implement store provider...

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,25 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
 import HomePage from "./components/pages/HomePage";
 import LoginPage from "./components/pages/LoginPage";
 import RegisterPage from "./components/pages/RegisterPage";
 import NavBar from "./components/NavBar";
+import "./index.css";
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <NavBar />
-      <div className="container mx-auto p-4">
+    <Provider store={store}>
+      <Router>
+        <NavBar />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
         </Routes>
-      </div>
-    </Router>
+      </Router>
+    </Provider>
   );
 };
 
