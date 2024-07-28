5a4eb93226df0751312d883f6a4c868593dd7fe1
Sylla <ismailasyllaa@gmail.com>
2024-07-29 01:45:06 +0400

    implement server test coverage

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,15 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    moduleNameMapper: {
-      '\\.(css|less)$': 'identity-obj-proxy',
-    },
-  };
-  
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '\\.(css|less)$': 'identity-obj-proxy',
+  },
+  collectCoverage: true, // Enable coverage collection
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}', // Collect coverage from all TypeScript files in the src folder
+    '!src/**/*.d.ts', // Exclude TypeScript declaration files
+    '!src/**/*.test.{ts,tsx}', // Exclude test files
+  ],
+  coverageDirectory: 'coverage', // Output directory for coverage reports
+  coverageReporters: ['json', 'lcov', 'text', 'clover'], // Report formats
+};
