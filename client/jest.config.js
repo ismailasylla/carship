581057ed5ba61fdb289c09a38f16995efe22d4f7
Sylla <ismailasyllaa@gmail.com>
2024-07-30 08:28:53 +0400

    Implement client Jest configuration with coverage collection and thresholds

---
+++ client/jest.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'jsdom',
+    setupFilesAfterEnv: ['<rootDir>/client/src/setupTests.ts'],
+    moduleNameMapper: {
+      '\\.(css|less)$': 'identity-obj-proxy', 
+    },
+    collectCoverage: true,
+    collectCoverageFrom: [
+      'src/**/*.{ts,tsx}',
+      '!src/**/*.d.ts', 
+      '!src/**/*.test.{ts,tsx}', 
+    ],
+    coverageDirectory: 'coverage',
+    coverageReporters: ['json', 'lcov', 'text', 'clover'],
+    coverageThreshold: {
+      global: {
+        branches: 80, 
+        functions: 80,
+        lines: 80,     
+        statements: 80 
+      }
+    },
+  };
+  
